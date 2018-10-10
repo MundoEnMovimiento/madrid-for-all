7ac4090d58cd6edbd0e717b221b9e85cfe5f0ab3
Carlos Garcia Rodriguez <cuetumayu@gmail.com>
2018-10-10 22:31:02 +0200

    Added first draft of the result table

---
+++ public/data/result_table_model_json.js
@@ -0,0 +1,12 @@
+{
+    "data" : {
+      "type" : "jsonfile",
+      "filename" : "./data/lostinmadrid_data_json.js",
+      "record" : "organization"
+      "items" : [
+        {"name" : "OrgName", "nodename" : "OrgName"},
+        {"name" : "District", "nodename" : "District"},
+        {"name" : "PostalCode", "nodename" : "PostalCode"}
+      ]
+    }
+  }
