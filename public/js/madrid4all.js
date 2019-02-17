9f97af9a9055604e51370fee2819f8cd4cace90d
Carlos GR <cuetumayu@gmail.com>
2019-02-17 23:38:25 +0100

    Worked a little bit on fixing some bugs in the search

--- public/js/madrid4all.js
+++ public/js/madrid4all.js
@@ -4,6 +4,8 @@ var dbName = 'madrid4all.db';
 var db = new PouchDB(dbName);
 var map;
 var originalArrayOfLocations = [];
+var originalArrayOfCategories = [];
+var originalArrayOfServices = [];
 var arrayOfLocations = [];
 var markersOnMap = [];
 var selectedCategories = [];
@@ -35,7 +37,7 @@ function initPageContent(loadedLocations) {
   db.info().then(function (result) {
     if (result.doc_count === 0) {
       console.log('DB empty! Lets add data.json into it...');
-      db.bulkDocs(arrayOfLocations).then(function (result) {
+      db.bulkDocs(arrayOfLocations).then(function (innerResult) {
         console.log("Locations added successfully to DB. ");
         createDBIndexes();
       }).catch(function (err) {
@@ -50,11 +52,11 @@ function initPageContent(loadedLocations) {
 // callback to load menu content: categories, services, etc.
 function initMenuContent(loadedCategories) {
   console.log("initMenuContent...");
+  originalArrayOfCategories = loadedCategories;
   var innerHtmlCode = "";
   // load categories and services
   if (loadedCategories != null && loadedCategories.length > 0) {
     for (var i = 0; i < loadedCategories.length; i++) {
-      var arrayOfServices = loadedCategories[i].services;
       // TODO: replace second call to originalArrayOfLocations[i].categories by originalArrayOfLocations[i].categories.getLabel(language);
       innerHtmlCode += "<button id=\"" + loadedCategories[i].key + "\" onclick=\"onCategoryClick('" + loadedCategories[i].key + "')\" class=\"w3-button w3-block w3-hover-gray w3-medium w3-left-align\">" + loadedCategories[i][selectedLanguage] + "</button>";
       innerHtmlCode += "<div id=\"" + loadedCategories[i].key + "-child\" class=\"w3-hide\"></div>";
@@ -63,6 +65,7 @@ function initMenuContent(loadedCategories) {
     document.getElementById('categories').innerHTML = innerHtmlCode;
     // load services
     w3.getHttpObject("./data/services.json", function (loadedServices) {
+      originalArrayOfServices = loadedServices;
       if (loadedServices != null && loadedServices.length > 0) {
         for (var i = 0; i < loadedServices.length; i++) {
           // console.log("category: " + loadedServices[i].category + ", service: " + loadedServices[i].key);
@@ -122,7 +125,7 @@ function addSingleLocationToMap(markerData) {
   // create an infoWindow to be linked to the marker
   var infoWindowCode = "<strong>" + markerData.orgName + "</strong><br>";
   infoWindowCode += "<em>" + address + "</em><br><a href=\"" + markerData.orgWeb + "\">" + markerData.orgWeb + "</a><br>";
-  infoWindowCode += "<a href=\"javascript:void(0)\" onClick=\"showLocationDetails('loc-details-" + markerData.ID + "')\">Mostrar más información</a><br>";
+  infoWindowCode += "<a href=\"#\" onClick=\"showLocationDetails('loc-details-" + markerData.ID + "')\">Mostrar más información</a><br>";
   //console.log("infoWindowCode: " + infoWindowCode); 
   var infowindow = new google.maps.InfoWindow({
     content: infoWindowCode
@@ -164,12 +167,21 @@ function onCategoryClick(selectedCategory) {
   if (selectedCategories.includes(selectedCategory)) {
     // unselect category and search again
     selectedCategories.pop(selectedCategory);
-    findLocationsInDatabase();
+    // remove svc linked to that category from the search criteria
+    for(var i = 0; i < selectedServices.length; i++) {
+      // TODO: REMOVE ALL THE pop() by splice() !
+      var serviceCategory = selectedServices[i].charAt(3);
+      if(serviceCategory == selectedCategory) {
+        console.log("Removing service 'svc" + selectedServices[i] + ' from search criteria');
+        selectedServices.splice(i, 1);
+      }
+    }
   } else {
     // select category and search again
     selectedCategories.push(selectedCategory);
-    findLocationsInDatabase();
   }
+  // trigger the find operation
+  findLocationsInDatabase();
   // update the UI
   w3.toggleClass('#' + selectedCategory, 'w3-large');
   w3.toggleClass('#' + selectedCategory + "-child", 'w3-hide', 'w3-show');
@@ -180,14 +192,14 @@ function onServiceClick(selectedService) {
   if (selectedServices.includes(selectedService)) {
     // unselect service and search again
     selectedServices.pop(selectedService);
-    findLocationsInDatabase();
   } else {
     // select service and search again
     selectedServices.push(selectedService);
-    findLocationsInDatabase();
   }
+  // trigger the find operation
+  findLocationsInDatabase();
   // update the UI
-  w3.toggleClass('#' + selectedCategory, 'w3-large');
+  w3.toggleClass('#' + selectedService, 'w3-large');
 }
 // sets the map on all the displayed markers
 function setMapOnAllMarkers(map) {
@@ -206,13 +218,23 @@ function findLocationsInDatabase() {
   // TODO: Rename ID in the Json and Excel file to key or something less similar to _id
   // compose selector based on the user input
   var searchCriterias = new Object();
-  // filter by categories
-  if (selectedCategories.length > 0) {
-    searchCriterias.categories = { $elemMatch: { $in: selectedCategories } };
+  var processedCategories = selectedCategories;
+  var processedServices = selectedServices;
+  // remove categories of any of their services are included in the search criteria
+  for(var i = 0; i < selectedServices.length; i++){
+    var serviceCategory = selectedServices[i].charAt(3);
+    // remove the category of this service from the searchCriteria
+    if(processedCategories.includes("cat" + serviceCategory)){
+        console.log("Removing category 'cat" + serviceCategory + "' from the search criteria as '" + selectedServices[i] + "' is added");
+        processedCategories.pop(serviceCategory);
+    }
   }
-  // filter by services
-  if (selectedServices.length > 0) {
-    searchCriterias.services = { $elemMatch: { $in: selectedServices } };
+  // compose the final search criteria
+  if(processedCategories.length > 0) {
+    searchCriterias.categories = { $elemMatch: { $in: processedCategories } };
+  }
+  if(processedServices.length > 0) {
+    searchCriterias.services = { $elemMatch: { $in: processedServices } };
   }
   // TODO: Add more filters
   // perform the find in the DB
@@ -247,11 +269,14 @@ function onRowClick(markerId) {
 function onResetClick() {
   console.log("Clicked on reset.");
   //document.getElementById("searchText").textContent = "";
-  w3.removeClass('.w3-bold', 'w3-bold');
+  w3.removeClass('.w3-large', 'w3-large');
   w3.toggleClass('.w3-show', 'w3-show', 'w3-hide');
   // clear previous results
   clearPreviousResults();
   initPageContent(originalArrayOfLocations);
+  // clean selected criterias
+  selectedCategories = [];
+  selectedServices = [];
 }
 // script to open sidebar
 function w3_open() {
