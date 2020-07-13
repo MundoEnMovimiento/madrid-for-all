5604779e044e5c4d05315cd4f017b36ac506e4e7
Carlos GR <cuetumayu@gmail.com>
2020-07-13 23:24:54 +0200

    New version of data.xlsx and their associated json versions

--- excel-to-json/excel-to-json-test.js
+++
@@ -1,5 +0,0 @@
-console.log("Processing row.xlsx...");
-convertExcel = require('excel-as-json').processFile;
-convertExcel("row.xlsx", "row.json", {sheet: '1', omitEmptyFields: true}, null);
-
-console.log("Finished!");
